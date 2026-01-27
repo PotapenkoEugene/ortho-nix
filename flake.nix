f8746cee778134a44cf4f40445ca5b0e0c5e33ba
PotapenkoEugene <g.r.e.e.d@yandex.ru>
2026-01-27 20:51:45 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Home Manager configuration of ortho";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Add nixgl
+    nixgl = {
+      url = "github:nix-community/nixGL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # Home manager
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
+    # url = "github:nix-community/nixvim/nixos-24.11";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+	};
+    };
+
+  outputs =
+    { nixpkgs, home-manager, nixvim, nixgl, ... }:
+    let
+      system = "x86_64-linux";
+	# pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          nixgl.overlay
+		    ];
+			    };
+    in
+    {
+      homeConfigurations."ortho" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ 
+		nixvim.homeManagerModules.nixvim
+		./home.nix 
+		];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+}
