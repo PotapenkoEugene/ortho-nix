3810ff1553e592c44db7bf40d15fef4101d135d7
PotapenkoEugene <g.r.e.e.d@yandex.ru>
2026-02-25 17:24:51 +0200

    remove no needed comments

--- home.nix
+++ home.nix
@@ -37,16 +37,6 @@
   nixpkgs.config.allowUnfree = true;
 
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
   home.activation.copyDesktopFiles = lib.hm.dag.entryAfter ["installPackages"] ''
